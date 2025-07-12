9eb53006913aed61ca5f5f5f4e51e10580d420d0
曾庆丰 <793396328@qq.com>
2025-07-12 22:07:08 +0800

    更新包

--- Packages/cn.etetet.yiuiframework/Runtime/Core/YIUIBase/Helper/UIOperationHelper.cs
+++ Packages/cn.etetet.yiuiframework/Runtime/Core/YIUIBase/Helper/UIOperationHelper.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Object = UnityEngine.Object;
 #if UNITY_EDITOR
 using ET;
@@ -41,7 +42,10 @@ namespace YIUIFramework
             if (IsPlaying())
             {
                 if (log)
+                {
                     Debug.LogError($"当前正在运行时 请不要在运行时使用");
+                }
+
                 return false;
             }
 
@@ -72,7 +76,10 @@ namespace YIUIFramework
             if (IsPlaying())
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowError($"当前正在运行时 请不要在运行时使用");
+                }
+
                 return false;
             }
 
@@ -84,7 +91,10 @@ namespace YIUIFramework
             if (IsPlaying())
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"当前正在运行时 请不要在运行时使用");
+                }
+
                 return false;
             }
 
@@ -92,7 +102,10 @@ namespace YIUIFramework
             if (checkInstance)
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"不能对实体进行操作  必须进入预制体编辑!!!");
+                }
+
                 return false;
             }
 
@@ -104,14 +117,20 @@ namespace YIUIFramework
             if (IsPlaying())
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"当前正在运行时 请不要在运行时使用");
+                }
+
                 return false;
             }
 
             if (obj == null)
             {
                 if (log)
+                {
                     Debug.LogError($"传入的对象为空");
+                }
+
                 return false;
             }
 
@@ -119,7 +138,10 @@ namespace YIUIFramework
             if (checkInstance)
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"不能对实体进行操作  必须进入预制体编辑!!!");
+                }
+
                 return false;
             }
 
@@ -127,7 +149,10 @@ namespace YIUIFramework
             if (!checkAsset)
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"1: 必须是预制体 2: 不能在Hierarchy面板中使用 必须在Project面板下的预制体原件才能使用使用 ");
+                }
+
                 return false;
             }
 
@@ -140,7 +165,10 @@ namespace YIUIFramework
             if (IsPlaying())
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"当前正在运行时 请不要在运行时使用");
+                }
+
                 return false;
             }
 
@@ -148,7 +176,10 @@ namespace YIUIFramework
             if (checkInstance)
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"不能对实体进行操作  必须进入预制体编辑!!!");
+                }
+
                 return false;
             }
 
@@ -156,7 +187,10 @@ namespace YIUIFramework
             if (!checkAsset)
             {
                 if (log)
+                {
                     UnityTipsHelper.ShowErrorContext(obj, $"1: 必须是预制体 2: 不能在Hierarchy面板中使用 必须在Project面板下的预制体原件才能使用使用 ");
+                }
+
                 return false;
             }
 
@@ -176,28 +210,33 @@ namespace YIUIFramework
             return GetETPackagesName(prefabPath, log);
         }
 
+        const string PackagesToken = "Packages/";
+        const string ETFormat = "cn.etetet";
+
         public static string GetETPackagesName(string path, bool log = true)
         {
-            if (path.Contains(YIUIConstHelper.Const.UIPackages) && path.Contains(YIUIConstHelper.Const.UIETPackagesFormat))
-            {
-                var pathSplit = path.Split('/');
+            path = path.Replace('\\', '/');
 
-                for (int i = 0; i < pathSplit.Length; i++)
+            if (path.Contains(PackagesToken) && path.Contains(ETFormat))
+            {
+                var packagesIndex = path.IndexOf(PackagesToken, StringComparison.Ordinal);
+                if (packagesIndex >= 0)
                 {
-                    var name = pathSplit[i];
-                    if (name.Contains(YIUIConstHelper.Const.UIPackages))
-                    {
-                        var packageFullName      = pathSplit[i + 1];
-                        var packageFullNameSplit = packageFullName.Split('.');
-                        var packageName          = packageFullNameSplit[^1];
-                        return packageName;
-                    }
+                    var startIndex = packagesIndex + PackagesToken.Length;
+                    var endIndex = path.IndexOf('/', startIndex);
+
+                    var packageFullName = endIndex > 0
+                            ? path.Substring(startIndex, endIndex - startIndex)
+                            : path.Substring(startIndex);
+
+                    var lastDotIndex = packageFullName.LastIndexOf('.');
+                    return lastDotIndex >= 0 ? packageFullName.Substring(lastDotIndex + 1) : packageFullName;
                 }
             }
 
             if (log)
             {
-                Log.Error($"{path} 未找到预制体的包名 请检查预制体路径是否正确");
+                Log.Error($"{path} 未找到包名");
             }
 
             return "";
