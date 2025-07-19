0b39006be8758262cbf8bab865685e0cb04092c2
曾庆丰 <793396328@qq.com>
2025-07-19 13:56:01 +0800

    【导入包】audio

---
+++ Packages/cn.etetet.yiuiaudio/Runtime/Audio/AudioClipLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using YIUIFramework;
+
+namespace ET
+{
+    [EnableClass]
+    public class AudioClipLoader : IAudioClipLoader
+    {
+        private readonly EntityRef<Entity> m_EntityRef;
+
+        public AudioClipLoader(Entity entity)
+        {
+            m_EntityRef = entity;
+        }
+
+        public void UnloadAudioClip(AudioClip clip)
+        {
+            EventSystem.Instance?.YIUIInvokeEntitySync(m_EntityRef, new YIUIInvokeEntity_Release { obj = clip });
+        }
+
+        public void LoadAudioClip(string clipName, Action<AudioClip> loadCompleted)
+        {
+            var audioClip = EventSystem.Instance?.YIUIInvokeEntitySync<YIUIInvokeLoadAudioClip, AudioClip>(m_EntityRef, new YIUIInvokeLoadAudioClip
+            {
+                ResName = clipName,
+            });        
+
+            if (audioClip == null)
+            {
+                Debug.LogError($"音频: {clipName} 没有找到!");
+                return;
+            }
+
+            loadCompleted?.Invoke(audioClip);
+        }
+    }
+}
